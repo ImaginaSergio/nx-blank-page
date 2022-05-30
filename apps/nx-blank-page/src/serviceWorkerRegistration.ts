f5f51957a678db4a77302c25098772e8c0bf9fb9
Sergio Cerdá Hervás <sergio@imaginagroup.com>
2022-05-30 10:01:13 +0200

    debug

--- apps/nx-blank-page/src/serviceWorkerRegistration.ts
+++ apps/nx-blank-page/src/serviceWorkerRegistration.ts
@@ -33,6 +33,9 @@ export function register(config?: Config) {
   ) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+
+    console.log('Probando URL Registro', { url: publicUrl });
+
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
